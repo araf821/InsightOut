3327b92b1f09b1b7eedf211727b87b60244babc3
araf821 <araf.ahmed200@gmail.com>
2023-05-02 23:43:42 -0400

    implemented hero component, for now...

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,14 +2,17 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="flex justify-center max-w-[2520px] mx-auto 2xl:px-20 ">
+    <div className="flex justify-center max-w-[2520px] mx-auto 2xl:px-20">
       <div
-        className="aspect-video relative overflow-hidden lg:rounded-b-xl text
-        flex flex-row justify-center h-[400px] sm:h-[700px] lg:h-[60vw] max-h-[800px] "
+        className="text-center aspect-video relative overflow-hidden xl:rounded-b-xl text
+        flex flex-row justify-center items-center h-[400px] sm:h-[700px] lg:h-[60vw] max-h-[800px] "
       >
+        <div className="max-w-[650px] z-10 text-5xl font-bold tracking-wider sm:rounded-lg px-4 py-2 sm:text-7xl md:text-8xl bg-black/10 text-white">
+          Unleash Your Insights
+        </div>
         <Image
           fill
-          src="/images/bg-1.jpg"
+          src="/images/bg-2.jpg"
           alt="blog background"
           className="object-cover w-full"
         />
--- app/components/categories/Categories.tsx
+++ app/components/categories/Categories.tsx
@@ -3,19 +3,19 @@ import CategoryItem from "./CategoryItem";
 
 const Categories = () => {
   return (
-    <Container>
-      <div className="flex flex-row justify-center items-center overflow-x-auto py-4">
-        <div className="flex flex-grow justify-center gap-8 md:gap-16 lg:gap-32 xl:gap-40">
-          <CategoryItem />
-          <CategoryItem />
-          <CategoryItem />
-          <CategoryItem />
-          <CategoryItem />
-        </div>
+    <section className="flex justify-center items-center max-w-[1425px] mx-auto xl:px-20 pt-4">
+      <div className="flex flex-row gap-8 overflow-auto px-4 lg:gap-12 xl:gap-16">
+        <CategoryItem />
+        <CategoryItem />
+        <CategoryItem />
+        <CategoryItem />
+        <CategoryItem />
+        <CategoryItem />
       </div>
-    </Container>
+    </section>
   );
 };
+
 export default Categories;
 
 export const categories = [];
