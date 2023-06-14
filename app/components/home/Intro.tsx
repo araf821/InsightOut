150c8f5e7982b332cda15828669b75df8b2d740c
araf821 <araf.ahmed200@gmail.com>
2023-06-13 21:25:52 -0400

    implemented intro section

--- app/components/home/Intro.tsx
+++ app/components/home/Intro.tsx
@@ -4,20 +4,22 @@ import SocialProof from "./SocialProof";
 
 const Intro = () => {
   return (
-    <section className="w-full pb-12 pt-5">
+    <section className="w-full pb-12 pt-10">
       <Container>
-        <div className="flex w-full flex-col gap-4">
-          <p className="font-merri text-[60px] font-semibold">
-            Welcome to the Insight<span className="text-accent">Out</span> Blog
-          </p>
-          <hr className="-mt-8 w-20 border-4 border-accent" />
-          <p className="text-3xl">We&rsquo;re more than just a blog.</p>
-          <p className="text-center text-lg">Let the numbers speak for us.</p>
-          <hr className="mx-auto -mt-3 w-20 border-zinc-400" />
-
-          {/* Social Proof Section */}
-          <div className="flex flex-col items-center justify-center gap-12 lg:flex-row">
-            <div className="flex w-full lg:w-fit flex-col justify-center gap-12 md:flex-row">
+        <div className="flex w-full flex-col gap-4 text-zinc-800 lg:flex-row lg:gap-12 xl:gap-0">
+          <div className="flex w-full flex-col items-center justify-center gap-4 ">
+            <p className="text-center font-merri text-4xl font-semibold sm:text-5xl md:text-6xl xl:text-[68px]">
+              Welcome to the Insight<span className="text-accent">Out</span>{" "}
+              Blog
+            </p>
+            <hr className="mx-auto -mt-0 w-20 border-4 border-accent lg:mx-0" />
+            <p className="-mt-2 text-center text-xl sm:text-2xl md:text-3xl lg:text-left">
+              We&rsquo;re more than just a blog.
+            </p>
+          </div>
+          <div className="flex w-full flex-col items-center gap-4">
+            {/* Social Proof Section */}
+            <div className="flex w-full max-w-[600px] flex-col items-center justify-center gap-6 lg:gap-8">
               <SocialProof
                 icon={FaTwitter}
                 followers="123,123,123"
@@ -28,12 +30,12 @@ const Intro = () => {
                 followers="123,123,123"
                 type="Likes"
               />
+              <SocialProof
+                icon={FaYoutube}
+                followers="123,123,123"
+                type="Subscribers"
+              />
             </div>
-            <SocialProof
-              icon={FaYoutube}
-              followers="123,123,123"
-              type="Subscribers"
-            />
           </div>
         </div>
       </Container>
--- app/components/texts/Slogan.tsx
+++ app/components/texts/Slogan.tsx
@@ -1,17 +1,11 @@
 import { FC } from "react";
-import { Nunito } from "next/font/google";
-
-const nunito = Nunito({
-  weight: "400",
-  subsets: ["latin"],
-});
 
 interface SloganProps {}
 
 const Slogan: FC<SloganProps> = ({}) => {
   return (
     <div
-      className={`${nunito.className} sm:text-md text-sm text-zinc-800 md:text-lg lg:text-xl`}
+      className={`sm:text-md font-ubuntu text-sm text-zinc-800 md:text-lg lg:text-xl`}
     >
       Unleash Your Insights
     </div>
