340dbd11b58d901b4bbc872f2e92fc8b36fa76c1
araf821 <araf.ahmed200@gmail.com>
2023-06-13 20:52:20 -0400

    implemented social proof section

--- app/components/home/Intro.tsx
+++ app/components/home/Intro.tsx
@@ -1,9 +1,10 @@
-import { FaTwitter } from "react-icons/fa";
+import { FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa";
 import Container from "../Container";
+import SocialProof from "./SocialProof";
 
 const Intro = () => {
   return (
-    <section className="w-full py-6">
+    <section className="w-full pb-12 pt-5">
       <Container>
         <div className="flex w-full flex-col gap-4">
           <p className="font-merri text-[60px] font-semibold">
@@ -12,33 +13,27 @@ const Intro = () => {
           <hr className="-mt-8 w-20 border-4 border-accent" />
           <p className="text-3xl">We&rsquo;re more than just a blog.</p>
           <p className="text-center text-lg">Let the numbers speak for us.</p>
+          <hr className="mx-auto -mt-3 w-20 border-zinc-400" />
 
           {/* Social Proof Section */}
-          <div className="flex flex-col justify-center md:flex-row gap-8">
-            {/* <SocialProof icon={} followers="" /> */}
-            <div className="flex gap-8 rounded-lg bg-primary px-6 py-3">
-              <FaTwitter className="text-7xl" />
-              <div className="flex flex-col justify-center">
-                <p className="text-2xl">123,321,123</p>
-                <p className="text-xl">Followers</p>
-              </div>
+          <div className="flex flex-col items-center justify-center gap-12 lg:flex-row">
+            <div className="flex w-full lg:w-fit flex-col justify-center gap-12 md:flex-row">
+              <SocialProof
+                icon={FaTwitter}
+                followers="123,123,123"
+                type="Followers"
+              />
+              <SocialProof
+                icon={FaFacebook}
+                followers="123,123,123"
+                type="Likes"
+              />
             </div>
-
-            <div className="flex gap-8 rounded-lg bg-primary px-6 py-3">
-              <FaTwitter className="text-7xl" />
-              <div className="flex flex-col justify-center">
-                <p className="text-2xl">123,321,123</p>
-                <p className="text-xl">Followers</p>
-              </div>
-            </div>
-
-            <div className="flex gap-8 rounded-lg bg-primary px-6 py-3">
-              <FaTwitter className="text-7xl" />
-              <div className="flex flex-col justify-center">
-                <p className="text-2xl">123,321,123</p>
-                <p className="text-xl">Followers</p>
-              </div>
-            </div>
+            <SocialProof
+              icon={FaYoutube}
+              followers="123,123,123"
+              type="Subscribers"
+            />
           </div>
         </div>
       </Container>
--- app/components/home/SocialProof.tsx
+++ app/components/home/SocialProof.tsx
@@ -4,12 +4,20 @@ import { IconType } from "react-icons";
 interface SocialProofProps {
   icon: IconType;
   followers: string;
+  type: string;
 }
 
-const SocialProof: FC<SocialProofProps> = ({}) => {
-  return <div className="">
-
-  </div>;
+const SocialProof: FC<SocialProofProps> = ({ icon: Icon, followers, type }) => {
+  return (
+    <div className="relative z-10 flex w-full md:w-fit gap-8 rounded-lg bg-primary px-6 py-3 outline outline-4 outline-bg">
+      <span className="absolute left-2 top-2 -z-10 h-full w-full rounded-md bg-primary"></span>
+      <Icon className="text-7xl" />
+      <div className="flex flex-col justify-center">
+        <p className="text-2xl">{followers}</p>
+        <p className="text-xl">{type}</p>
+      </div>
+    </div>
+  );
 };
 
 export default SocialProof;
