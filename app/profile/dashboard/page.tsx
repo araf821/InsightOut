c5c92de1400cedceda31498f942a1b06d4e635f4
araf821 <araf.ahmed200@gmail.com>
2023-06-28 22:32:02 -0400

    implemented top level grid system + ui on dashbaord

--- app/profile/dashboard/page.tsx
+++ app/profile/dashboard/page.tsx
@@ -11,16 +11,16 @@ const Dashboard = async ({}) => {
     return null;
   }
 
-  const postsFromUser: Post[] = await getPostsByUser(currentUser?.id);
+  const postsFromUser: any[] = await getPostsByUser(currentUser?.id);
 
   return (
     <Container>
       <section className="py-8">
         <Heading title="Dashboard" />
         {/* Author Info */}
-        <div className="my-8 grid grid-cols-1 gap-8 lg:grid-cols-2">
+        <div className="my-8 grid grid-cols-1 gap-4  lg:grid-cols-5">
           {/* Main info */}
-          <div className="relative flex w-full overflow-hidden rounded-l-lg bg-secondary">
+          <div className="relative flex w-full overflow-hidden rounded-l-lg bg-secondary shadow-md lg:col-span-3">
             {/* Image component */}
             <div className="relative h-[100px] max-h-[200px] w-[100px] max-w-[200px] md:h-[200px] md:w-full">
               <Image
@@ -32,9 +32,34 @@ const Dashboard = async ({}) => {
               />
             </div>
 
-            <div className="flex w-full flex-col gap-2">
-              <p>{currentUser.name}</p>
-              <p>Posts published: {postsFromUser.length}</p>
+            <div className="flex w-full flex-col gap-3 px-3 py-4">
+              <p className="text-3xl font-semibold">
+                <span className="rounded-md bg-zinc-800 px-3 py-1.5 text-white ">
+                  {currentUser.name}
+                </span>
+              </p>
+              <p className="font-josefin text-xl">
+                Posts published: {postsFromUser.length}
+              </p>
+            </div>
+          </div>
+
+          {/* Followers/Following */}
+          <div className="relative flex h-full w-full flex-col items-center justify-between gap-2 font-josefin text-neutral-800 opacity-70 md:flex-row lg:col-span-2 lg:flex-col">
+            <div className="grid h-full w-full items-center rounded-md bg-secondary px-6 text-5xl shadow-md">
+              <div className="flex translate-y-1 flex-row gap-8">
+                <span>0</span>
+                <div className="border-l-2 border-neutral-300" />
+                <span className="my-auto text-3xl">Followers</span>
+              </div>
+            </div>
+
+            <div className="grid h-full w-full items-center rounded-md bg-secondary px-6 text-5xl shadow-md">
+              <div className="flex flex-row gap-8">
+                <span>0</span>
+                <div className="border-l-2 border-neutral-300" />
+                <span className="my-auto text-3xl">Following</span>
+              </div>
             </div>
           </div>
         </div>
