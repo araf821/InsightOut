0e3750b2236c7eecf837d0b584c57e2fd6b8a38a
araf821 <araf.ahmed200@gmail.com>
2023-06-29 23:42:26 -0400

    updated dashboard layouts, implemented date conversoin function

--- app/profile/dashboard/page.tsx
+++ app/profile/dashboard/page.tsx
@@ -2,8 +2,20 @@ import getCurrentUser from "@/app/actions/getCurrentUser";
 import getPostsByUser from "@/app/actions/getPostsByUser";
 import Container from "@/app/components/Container";
 import Heading from "@/app/components/Heading";
-import { Post } from "@prisma/client";
 import Image from "next/image";
+
+type PostWithoutUser = {
+  createdAt: string;
+  updatedAt: string;
+  id: string;
+  title: string;
+  content: string;
+  slug: string;
+  image: string;
+  tags: string[];
+  published: boolean;
+  authorId: string;
+};
 
 const Dashboard = async ({}) => {
   const currentUser = await getCurrentUser();
@@ -11,36 +23,97 @@ const Dashboard = async ({}) => {
     return null;
   }
 
-  const postsFromUser: any[] = await getPostsByUser(currentUser?.id);
+  //@ts-ignore
+  const postsFromUser: PostWithoutUser[] = await getPostsByUser(currentUser.id);
+
+  const formatDate = (date: string) => {
+    const [year, month, day] = date.split("-");
+    let dateToString = "";
+
+    switch (month) {
+      case "01":
+        dateToString = "Jan";
+        break;
+
+      case "02":
+        dateToString = "Feb";
+        break;
+
+      case "03":
+        dateToString = "Mar";
+        break;
+
+      case "04":
+        dateToString = "Apr";
+        break;
+
+      case "05":
+        dateToString = "May";
+        break;
+
+      case "06":
+        dateToString = "Jun";
+        break;
+
+      case "07":
+        dateToString = "Jul";
+        break;
+
+      case "08":
+        dateToString = "Aug";
+        break;
+
+      case "09":
+        dateToString = "Sept";
+        break;
+
+      case "10":
+        dateToString = "Oct";
+        break;
+
+      case "11":
+        dateToString = "Nov";
+        break;
+
+      case "12":
+        dateToString = "Dec";
+        break;
+
+      default:
+        break;
+    }
+
+    return `${dateToString} ${day}, ${year}`;
+  };
 
   return (
     <Container>
       <section className="py-8">
         <Heading title="Dashboard" />
         {/* Author Info */}
-        <div className="my-8 grid grid-cols-1 gap-4  lg:grid-cols-5">
+        <div className="my-8 grid grid-cols-1 gap-4 lg:grid-cols-5">
           {/* Main info */}
-          <div className="relative flex w-full overflow-hidden rounded-l-lg bg-secondary shadow-md lg:col-span-3">
+          <div className="relative flex w-full flex-col overflow-hidden rounded-t-lg bg-secondary px-2 py-3 shadow-md sm:flex-row sm:rounded-l-lg lg:col-span-3">
             {/* Image component */}
-            <div className="relative h-[100px] max-h-[200px] w-[100px] max-w-[200px] md:h-[200px] md:w-full">
+            <div className="relative  aspect-square w-full sm:max-w-[200px] md:h-[200px]">
               <Image
                 //@ts-ignore
                 src={currentUser?.image || "/images/placeholder.jpg"}
                 alt="current user profile"
                 fill
-                className="h-[200px] object-cover"
+                className="rounded-md object-cover"
               />
             </div>
 
-            <div className="flex w-full flex-col gap-3 px-3 py-4">
-              <p className="text-3xl font-semibold">
-                <span className="rounded-md bg-zinc-800 px-3 py-1.5 text-white ">
-                  {currentUser.name}
-                </span>
+            <div className="flex w-full flex-col gap-2 py-4 font-josefin sm:px-2 sm:py-0">
+              <p className="font-merri text-2xl font-semibold">
+                {currentUser.name}
               </p>
-              <p className="font-josefin text-xl">
-                Posts published: {postsFromUser.length}
+              <p className="text-xl">
+                Member Since: {formatDate(currentUser.createdAt.split("T")[0])}
               </p>
+              <p className="text-xl">Posts published: {postsFromUser.length}</p>
+              <p className="text-xl">Posts published: {postsFromUser.length}</p>
             </div>
           </div>
 
