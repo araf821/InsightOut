da6abcd8bc83f58cab0ec0f948f8b0dd207826e5
araf821 <araf.ahmed200@gmail.com>
2023-07-04 23:59:55 -0400

    added access to profile settings from dashboard

---
+++ app/profile/dashboard/DashboardClient.tsx
@@ -0,0 +1,136 @@
+"use client";
+
+import Button from "@/app/components/Button";
+import Heading from "@/app/components/Heading";
+import PostCard from "@/app/components/PostCard";
+import { dateFormat } from "@/app/lib/helpers/dateFormat";
+import { SafePost } from "@/app/types";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { FC } from "react";
+import { BsPenFill } from "react-icons/bs";
+import { FaUserEdit } from "react-icons/fa";
+
+interface DashboardClientProps {
+  postsFromUser: SafePost[];
+  userName: string | null;
+  userCreated: string;
+  userImage: string | null;
+}
+
+const DashboardClient: FC<DashboardClientProps> = ({
+  postsFromUser,
+  userName,
+  userCreated,
+  userImage,
+}) => {
+  const router = useRouter();
+
+  const published = postsFromUser.filter((post) => post.published);
+  const drafts = postsFromUser.filter((post) => !post.published);
+
+  return (
+    <section className="py-8">
+      <Heading title="Dashboard" />
+
+      <div className="my-8 grid grid-cols-1 gap-4 lg:grid-cols-5">
+        {/* Author Info */}
+        <div className="relative flex w-full flex-col overflow-hidden rounded-t-lg bg-secondary px-2 py-3 shadow-md sm:flex-row sm:rounded-l-lg lg:col-span-3">
+          <span
+            onClick={() => router.push("/profile/preferences")}
+            className="cursor-pointer absolute right-1 top-2 z-10 grid h-12 w-12 group place-items-center sm:top-1"
+          >
+            <FaUserEdit className="opacity-80 group-hover:opacity-100 group-hover:scale-110 transition text-2xl text-neutral-200 sm:text-neutral-800" />
+          </span>
+          {/* Image component */}
+          <div className="relative  aspect-square w-full sm:max-w-[200px] md:h-[200px]">
+            <Image
+              //@ts-ignore
+              src={userImage || "/images/placeholder.jpg"}
+              alt="current user profile"
+              fill
+              className="rounded-md object-cover"
+            />
+          </div>
+
+          <div className="flex w-full flex-col gap-2 pt-4 font-josefin sm:px-2 sm:py-0">
+            <p className="font-merri text-2xl font-semibold">{userName}</p>
+            <hr className="border-neutral-300" />
+            <p className="text-xl font-light">
+              Member Since: {dateFormat(userCreated)}
+            </p>
+            <p className="text-xl font-light">
+              Posts published: {postsFromUser.length}
+            </p>
+          </div>
+        </div>
+
+        {/* Followers/Following Section*/}
+        <div className="relative flex h-full w-full cursor-not-allowed flex-col items-center justify-between gap-2 font-josefin text-neutral-800 md:flex-row lg:col-span-2 lg:flex-col">
+          <div className="absolute right-0 top-0 z-10 rounded-bl-md rounded-tr-md bg-secondary px-2 py-1 text-zinc-800 shadow-md md:text-base">
+            Coming Soon
+          </div>
+          <div className="blur-sms grid h-full w-full items-center rounded-md bg-secondary px-6 opacity-50 shadow-md">
+            <div className="flex translate-y-1 flex-row gap-8 py-4">
+              <span className="text-xl md:text-2xl lg:text-3xl">0</span>
+              <div className="border-l-2 border-neutral-300" />
+              <span className="my-auto text-xl sm:text-2xl md:text-3xl">
+                Followers
+              </span>
+            </div>
+          </div>
+
+          <div className="blur- m grid h-full w-full items-center rounded-md bg-secondary px-6 opacity-50 shadow-md">
+            <div className="flex translate-y-1 flex-row gap-8 py-4 md:py-0">
+              <span className="text-xl md:text-2xl lg:text-3xl">0</span>
+              <div className="border-l-2 border-neutral-300" />
+              <span className="my-auto text-xl sm:text-2xl md:text-3xl">
+                Following
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Displaying User's Posts */}
+      {published.length > 0 && (
+        <>
+          <Heading small center title="Published" />
+          <section className="grid grid-cols-1 gap-4 py-8 md:grid-cols-2 lg:grid-cols-3">
+            {published.map((post) => (
+              <PostCard key={post.id} post={post} />
+            ))}
+          </section>
+        </>
+      )}
+
+      {drafts.length > 0 && (
+        <>
+          <Heading small center title="Drafts" />
+          <section className="grid grid-cols-1 gap-4 py-8 md:grid-cols-2 lg:grid-cols-3">
+            {drafts.map((post) => (
+              <PostCard key={post.id} post={post} />
+            ))}
+          </section>
+        </>
+      )}
+
+      {!drafts.length && !published.length && (
+        <section className="flex w-full flex-col items-center gap-4 py-12 ">
+          <p className="font-josefin text-lg font-semibold md:text-xl lg:text-2xl xl:text-3xl">
+            Looks like you have not written any posts yet.
+          </p>
+          <Button
+            onClick={() => {}}
+            label="Start Writing Now!"
+            className="max-w-[500px]"
+            icon={BsPenFill}
+            outline
+          />
+        </section>
+      )}
+    </section>
+  );
+};
+
+export default DashboardClient;
--- app/profile/dashboard/page.tsx
+++ app/profile/dashboard/page.tsx
@@ -1,11 +1,14 @@
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import getPostsByUser from "@/app/actions/getPostsByUser";
+import Button from "@/app/components/Button";
 import Container from "@/app/components/Container";
 import Heading from "@/app/components/Heading";
 import PostCard from "@/app/components/PostCard";
 import { dateFormat } from "@/app/lib/helpers/dateFormat";
 import { SafePost } from "@/app/types";
 import Image from "next/image";
+import { BsPen } from "react-icons/bs";
+import DashboardClient from "./DashboardClient";
 
 const Dashboard = async ({}) => {
   const currentUser = await getCurrentUser();
@@ -15,102 +18,15 @@ const Dashboard = async ({}) => {
 
   //@ts-ignore
   const postsFromUser: SafePost[] = await getPostsByUser(currentUser.id, 6);
-  const published = postsFromUser.filter((post) => post.published);
-  const drafts = postsFromUser.filter((post) => !post.published);
 
   return (
     <Container>
-      <section className="py-8">
-        <Heading title="Dashboard" />
-        {/* Author Info */}
-        <div className="my-8 grid grid-cols-1 gap-4 lg:grid-cols-5">
-          {/* Main info */}
-          <div className="relative flex w-full flex-col overflow-hidden rounded-t-lg bg-secondary px-2 py-3 shadow-md sm:flex-row sm:rounded-l-lg lg:col-span-3">
-            {/* Image component */}
-            <div className="relative  aspect-square w-full sm:max-w-[200px] md:h-[200px]">
-              <Image
-                //@ts-ignore
-                src={currentUser?.image || "/images/placeholder.jpg"}
-                alt="current user profile"
-                fill
-                className="rounded-md object-cover"
-              />
-            </div>
-
-            <div className="flex w-full flex-col gap-2 pt-4 font-josefin sm:px-2 sm:py-0">
-              <p className="font-merri text-2xl font-semibold">
-                {currentUser.name}
-              </p>
-              <hr className="border-neutral-300" />
-              <p className="text-xl font-light">
-                Member Since: {dateFormat(currentUser.createdAt)}
-              </p>
-              <p className="text-xl font-light">
-                Posts published: {postsFromUser.length}
-              </p>
-            </div>
-          </div>
-
-          {/* Followers/Following */}
-          <div className="relative flex h-full w-full cursor-not-allowed flex-col items-center justify-between gap-2 font-josefin text-neutral-800 md:flex-row lg:col-span-2 lg:flex-col">
-            <div className="absolute right-0 top-0 z-10 rounded-bl-md rounded-tr-md bg-secondary px-2 py-1 text-zinc-800 shadow-md md:text-base">
-              Coming Soon
-            </div>
-            <div className="blur-sms grid h-full w-full items-center rounded-md bg-secondary px-6 opacity-50 shadow-md">
-              <div className="flex translate-y-1 flex-row gap-8 py-4">
-                <span className="text-xl md:text-2xl lg:text-3xl">0</span>
-                <div className="border-l-2 border-neutral-300" />
-                <span className="my-auto text-xl sm:text-2xl md:text-3xl">
-                  Followers
-                </span>
-              </div>
-            </div>
-
-            <div className="blur- m grid h-full w-full items-center rounded-md bg-secondary px-6 opacity-50 shadow-md">
-              <div className="flex translate-y-1 flex-row gap-8 py-4 md:py-0">
-                <span className="text-xl md:text-2xl lg:text-3xl">0</span>
-                <div className="border-l-2 border-neutral-300" />
-                <span className="my-auto text-xl sm:text-2xl md:text-3xl">
-                  Following
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {published.length > 0 && (
-          <>
-            <Heading small center title="Published" />
-            <section className="grid grid-cols-1 gap-4 py-8 md:grid-cols-2 lg:grid-cols-3">
-              {published.map((post) => (
-                <PostCard key={post.id} post={post} />
-              ))}
-            </section>
-          </>
-        )}
-
-        {drafts.length > 0 && (
-          <>
-            <Heading small center title="Drafts" />
-            <section className="grid grid-cols-1 gap-4 py-8 md:grid-cols-2 lg:grid-cols-3">
-              {drafts.map((post) => (
-                <PostCard key={post.id} post={post} />
-              ))}
-            </section>
-          </>
-        )}
-
-        {!drafts.length && !published.length && (
-          <section className="flex w-full flex-col items-center gap-2 py-12 font-josefin">
-            <p className="text-lg font-semibold md:text-xl lg:text-2xl xl:text-3xl">
-              Looks like you have not written any posts yet.
-            </p>
-            <p className="cursor-pointer text-neutral-700 transition-colors hover:text-neutral-900 hover:underline md:text-lg lg:text-xl xl:text-2xl">
-              Get started now!
-            </p>
-          </section>
-        )}
-      </section>
+      <DashboardClient
+        userName={currentUser.name}
+        userImage={currentUser.image}
+        userCreated={currentUser.createdAt}
+        postsFromUser={postsFromUser}
+      />
     </Container>
   );
 };
